965359eb9b7a39db14aa06af5bf87096ef17a403
Josue-SR <santozjosue831@gmail.com>
2022-07-20 16:12:38 -0700

    added css to the seetings modal

--- client/components/Modal/SettingsModal.vue
+++ client/components/Modal/SettingsModal.vue
@@ -1,14 +1,33 @@
 <template>
   <Modal class="settings-modal" @close="close">
     <h1>Settings</h1>
+    <div class="buttons">
+      <button class="button">
+        <Icon name="sun" />Themes
+      </button>
+      <button class="button">
+        <Icon name="code" />Other
+      </button>
+      <button class="button">
+        <Icon name="code" />Other
+      </button>
+    </div>
   </Modal>
 </template>
 
 <script>
 export default {
 	data() {
-		return {
-			name: undefined
+		return {}
+	},
+	computed: {
+		currentSetting: {
+			set(value) {
+				this.$store.commit('SET_CURRENT_SETTING', value)
+			},
+			get() {
+				return this.$store.state.currentSetting
+			}
 		}
 	},
 	methods: {
@@ -25,6 +44,16 @@ export default {
     & h1 {
 			font-size: 1.2rem;
 			margin-bottom: 1rem;
+		}
+	& button {
+			display: flex;
+			align-items: center;
+			margin-top: 0.7rem;
+
+			& div {
+				margin-right: 0.3rem;
+				margin-left: -3px;
+			}
 		}
   }
 </style>
